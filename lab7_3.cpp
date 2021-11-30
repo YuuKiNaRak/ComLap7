599da48053ffeabdda09fa84bc02582c6b7f1c89
ํYuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2021-11-30 17:28:16 +0700

    Complete lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,49 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+    int x,y,i=0;
+    while(i == 0){
+        if(a >= 360 ){
+        a=a-360;
+    }
+    else{
+        i++;
+    }
+    }
+    i=0;
+    while(i == 0){
+        if(b >= 360 ){
+        b=b-360;
+    }
+    else{
+        i++;
+    }
+    }
+
+    x=a-b;
+    y=(360-b)+a;
+    if(x < 0){
+        x=x*-1;
+    }
+    if(y < 0){
+        y=y*-1;
+    }
+    if(x <= y){
+        return x;
+    }else{
+        return y;
+    }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
+  return 0;
 }
